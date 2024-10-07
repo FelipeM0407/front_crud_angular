72f47e093ff98e535e80b461afe84ba7d2cf619c
FelipeM0407 <felipemoreirasilva97@gmail.com>
2024-10-07 00:47:23 -0300

    implementation of componentes and services

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PeoplesComponent } from './components/peoples/peoples.component';
+
+export const routes: Routes = [{
+    path: 'peoples', component: PeoplesComponent
+}];
